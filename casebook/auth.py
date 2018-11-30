3caa7c69eb76116ace120b109fdb3d3d09142007
David Fan <dfan14051@gmail.com>
2018-11-30 00:21:06 -0500

    Post endpoints done

--- casebook/auth.py
+++ casebook/auth.py
@@ -1,14 +1,14 @@
 import functools
 
 from flask import (
-    Blueprint, g
+    Blueprint, g, redirect, url_for
 )
 
 
 bp = Blueprint('auth', __name__, url_prefix='/api')
 
 def login_required(view):
-    @functools.wrap(view)
+    @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
             return redirect(url_for('auth.login'))
---
+++ casebook/posts.py
@@ -0,0 +1,68 @@
+import time
+
+from flask import (
+    Blueprint, g, jsonify, redirect, request, url_for
+)
+
+from casebook.auth import login_required
+from casebook.db.db import get_full_db_connection
+from casebook.objects.postobject import PostObject
+
+bp = Blueprint('post', __name__, url_prefix='/api')
+
+@bp.route('/posts')
+@login_required
+def posts():
+    username = request.args.get("username")
+    conn = get_full_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT post.id, users.username, unix_timestamp(post.posted_time), post.title, post.body FROM post, users WHERE post.author_id = users.id AND users.username = %(username)s", {'username': username}) 
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    data = map(lambda x: PostObject(x).to_dict(), rows)
+    
+    return jsonify(posts=list(data))
+
+@bp.route('/post', methods=['POST'])
+@login_required
+def post():
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()) 
+    conn = get_full_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO post(author_id, posted_time, title, body) VALUES (%s, %s, %s, %s)", (g.user_id, timestamp, request.form['title'], request.form['body'])) 
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return ('', 204)
+
+@bp.route('/deletepost', methods=['DELETE'])
+@login_required
+def delete_post():
+    post_id = request.form['post_id']
+    conn = get_full_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM post WHERE post.id = %s AND post.author_id = %s", (post_id, g.user_id))
+    conn.commit()
+    
+    cursor.close()
+    conn.close()
+    
+    return ('', 204)
+
+@bp.route('/updatepost', methods=['PUT'])
+@login_required
+def update_post():
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime()) 
+    conn = get_full_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE post SET post.posted_time = %s, post.title = %s, post.body = %s WHERE post.id = %s AND post.author_id = %s", (timestamp, request.form['title'], request.form['body'], request.form['id'], 1))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return('', 204)
